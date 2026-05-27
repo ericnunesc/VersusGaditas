1f32f782cd2e95bcb23e293e535d2e7063aa9710
ericnunesc <>
2026-05-27 00:18:36 -0300

    Remove arquivos temporarios de debug

--- gerar-b64.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const b64 = fs.readFileSync('firebase-service-account.json').toString('base64');
-fs.writeFileSync('chave-vercel.txt', b64);
-console.log('Pronto! Arquivo chave-vercel.txt criado.');
